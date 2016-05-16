ed119a9f0b6466dc09b6deb854ecb8be8064ec6d
itgoesfun <itgoesfun@gmail.com>
2016-05-16 11:29:06 -0400

    Created ATM project. First commit.

---
+++ ApplicationTellerMachineMVC5Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplicationTellerMachineMVC5Demo.Startup))]
+namespace ApplicationTellerMachineMVC5Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
